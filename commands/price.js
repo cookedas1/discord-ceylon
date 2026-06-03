4e95765bf5b74b79e79f89e34ffeead5a2c6af60
cookedas1 <mulmando1991@gmail.com>
2026-06-03 12:21:30 +0900

    라이선스 관련 조항 추가

--- commands/price.js
+++ commands/price.js
@@ -1,3 +1,19 @@
+/** Copyright (C) 2026 HaniB Studio (nageune1010@gmail.com)
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+ 
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ComponentType } = require('discord.js');
 const db = require('../utils/db');
 const { handleError } = require('../utils/errorHandler'); // 💡 에러 핸들러 임포트
